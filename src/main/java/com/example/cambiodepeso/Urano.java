bbdca7abd1c32e54423d482add36788b98a1a306
Sergioahs <71783208+Sergioahs@users.noreply.github.com>
2021-03-02 09:48:02 -0600

    Agregar Clase main

---
+++ src/main/java/com/example/cambiodepeso/Urano.java
@@ -0,0 +1,30 @@
+package com.example.cambiodepeso;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Urano extends AppCompatActivity {
+    private TextView txtCalcPeso;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_Urano);
+        txtCalcPeso= (TextView)findViewById(R.id.txtCalcPeso);
+        Bundle extras = getIntent().getExtras();
+        double numero = extras.getDouble("peso");
+        double peso= numero/9.81*8.87;
+        String total= String.valueOf(peso);
+        txtCalcPeso.setText(total);
+    }
+
+    public void anterior(View view){
+        Intent anterior = new Intent(this, MainActivity.class);
+        startActivity(anterior);
+    }
+
+}
