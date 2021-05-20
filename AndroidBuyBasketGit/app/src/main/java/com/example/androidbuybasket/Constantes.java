16c74e1a83aea4fec33565ba3ae52b402931a8ab
lizardiOmar <71732399+lizardiOmar@users.noreply.github.com>
2021-05-20 18:47:59 -0500

    Delete AndroidBuyBasketGit/app directory

--- AndroidBuyBasketGit/app/src/main/java/com/example/androidbuybasket/Constantes.java
+++
@@ -1,8 +0,0 @@
-package com.example.androidbuybasket;
-
-public class Constantes {
-    public static final String claveEncriptacion = "secreto!";
-    public static final String urlCrearCliente = "http://192.168.1.102:80/Bbasket2021/agregar_cliente.php";
-    public static final String urlGetPassCliente = "http://192.168.1.102:80/Bbasket2021/getPass_byCorreo_cliente.php";
-    public static final String urlAccederCliente = "http://192.168.1.102:80/Bbasket2021/login_cliente.php";
-}
