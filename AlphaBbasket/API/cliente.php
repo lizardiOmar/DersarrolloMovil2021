793e13df42dc5fe72154ab8de843c47545b8abeb
lizardiOmar <71732399+lizardiOmar@users.noreply.github.com>
2021-05-24 22:59:07 -0500

    Add files via upload

--- AlphaBbasket/API/cliente.php
+++ AlphaBbasket/API/cliente.php
@@ -103,12 +103,62 @@
 				
 		}
 		
-		public function borrarCliente(){
+		public static function borrarCliente($correo){
+			$response = null;
+			$sql = "DELETE FROM cliente WHERE correo='$correo'";
+			$conn=new Conexion();
+			try {
+				$conn->getConexion()->exec($sql);
 				
+				if ($conn->query($sql) == TRUE) {
+					$response = array(
+						"estado"=>"TRUE",
+						"MENSAJE"=>"Cuenta eliminada"
+					);
+				} else{
+					$response = array(
+						"estado"=>"FALSE",
+						"MENSAJE"=>"Cuenta No eliminada"
+					);
+				}
+			} catch (PDOException $e) {
+				$response = array(
+					"estado"=>"FALSE",
+					"MENSAJE"=>$e
+				);
+			}
+			$conn=null;
+			echo json_encode($response);
 		}
 		
-		public function mostrarCliente(){
-				
+		public static function mostrarCliente($correo){
+			$response = null;
+			$conn=new Conexion();
+			$sql = "SELECT * FROM cliente where correo='$correo'";
+			try {
+				$stmt = $conn->getConexion()->query($sql);
+				$result = $stmt->setFetchMode(PDO::FETCH_NUM);
+				while ($row = $stmt->fetch()) {
+					$id = $row[0];
+					$nombres = $row[1];
+					$apellidos = $row[2];
+					$correo = $row[3];
+					$edad = $row[4];
+					$clave = $row[5];
+				}
+				$cliente =  new Cliente("$id", "$nombres", "$apellidos", "$correo", "$edad", "$clave");
+				$response = array(
+					"cliente"=>$cliente
+				);
+				echo json_encode($response);
+			} catch (PDOException $e) {
+				$response = null;
+				$response = array(
+					"estado"=>"FALLIDO",
+					"cliente"=>"NULL"
+				);
+				echo json_encode($response);
+			}
 		}
 	}
 	
--- AlphaBbasket/API/clientes.php
+++ AlphaBbasket/API/clientes.php
@@ -13,129 +13,29 @@
 			isset($_POST['correo']) &&
 			isset($_POST['edad']) &&
 			isset($_POST['clave'])) {
-				
 				$cliente =  new Cliente($_POST['id'], $_POST['nombres'], $_POST['apellidos'], $_POST['correo'], $_POST['edad'], $_POST['clave']);
-				$response = null;
-				try {	
-					$sql = "INSERT INTO cliente (id, nombres, apellidos, correo, edad, clave, fecha_alta)
-					VALUES ($cliente->id, '$cliente->nombres', '$cliente->apellidos', '$cliente->correo', $cliente->edad, '$cliente->clave', CURRENT_TIMESTAMP())";
-					$conexion->exec($sql);
-					$response = array(
-						"SERVICIO"=>$estado_servidor,
-						"ESTADO"=>"CREADO"
-					);
-				} catch(PDOException $e) {
-					$response = array(
-						"SERVICIO"=>$estado_servidor,
-						"ESTADO"=>"NO CREADO",
-						"ERROR"=>$e
-					);
-				}
-					
-				
-				
-			}else{
-				$response = array(
-						"SERVICIO"=>"ERROR",
-						"ESTADO"=>"DATA NULA"
-				);
+				$cliente->guardarCliente();
 			}
-			$conexion = null;
-			echo json_encode($response);
 		break;
 		//Leer clientes o cliente GET
 		case 'GET':
-		$response = null;
-		if (isset($_GET['correo'])) {
-			$correo = $_GET['correo'];
-			$sql = "SELECT * FROM cliente where correo='$correo'";
-			try {
-				$stmt = $conexion->query($sql);
-				$result = $stmt->setFetchMode(PDO::FETCH_NUM);
-				while ($row = $stmt->fetch()) {
-					$id = $row[0];
-					$nombres = $row[1];
-					$apellidos = $row[2];
-					$correo = $row[3];
-					$edad = $row[4];
-					$clave = $row[5];
-				}
-			$cliente =  new Cliente("$id", "$nombres", "$apellidos", "$correo", "$edad", "$clave");
-			
-			$response = array(
-				"cliente"=>$cliente
-			);
-			echo json_encode($response);
-		} catch (PDOException $e) {
-			$response = array(
-				"estado"=>"FALLIDO",
-				"cliente"=>"NULL"
-			);
-			echo json_encode($response);
-		}
-	}else{
-		
-		$cliente =  new Cliente("90", "Omar", "Alonso", "correo_electronico", "edad", "clave");
-		$cliente->guardarCliente();
-		
-	}	
+			if (isset($_GET['correo'])) {
+				$correo = $_GET['correo'];
+				Cliente::mostrarCliente('$correo');
+			}
 		break;
 		//Actualizar cliente UPDATE
 		case 'PUT':
-		if(isset($_POST['nombres']) &&
-			isset($_POST['apellidos']) &&
-			isset($_POST['edad'])&&
-			isset($_POST['correo'])){
-				$nombres = $_POST['nombres'];
-				$apellidos = $_POST['apellidos'];
-				$correo = $_POST['correo'];
-				$edad = $_POST['edad'];
-				$sql = "UPDATE cliente SET nombres='$nombres', apellidos='$apellidos', edad=$edad WHERE correo='$correo';";
-				if ($conexion->query($sql) === TRUE) {
-					echo "Record updated successfully";
-				} else {
-					echo "Error updating record: internal" ;
-				}
-			}else{
-				echo "Error updating record: external" ;
-			}
-		
+			
 		break;
 		//Eliminar cliente DELETE
 		case 'DELETE':
-		$response = null;
-		if(isset($_GET['correo'])){
-		$correo=$_GET['correo'];
-		
-		try {
-			$sql = "DELETE FROM cliente WHERE correo='$correo'";
-			$conexion->exec($sql);
-			
-			if ($conexion->query($sql) == TRUE) {
-				$response = array(
-					"estado"=>"TRUE",
-					"MENSAJE"=>"Cuenta eliminada"
-				);
-			} else{
-				$response = array(
-					"estado"=>"FALSE",
-					"MENSAJE"=>"Cuenta No eliminada"
-				);
+			if(isset($_GET['correo'])){
+			$correo=$_GET['correo'];
+				Cliente::borrarCliente($correo);
 			}
-		} catch (PDOException $e) {
-			$response = array(
-				"estado"=>"FALSE",
-				"MENSAJE"=>$e
-			);
-		}
-	}else{
-		$response = array(
-			"estado"=>"FALSE",
-			"MENSAJE"=>"CLIENTE NULO"
-		);
-	}
-	
-	echo json_encode($response);
+		
+			
 		break;
 	}
 ?>
