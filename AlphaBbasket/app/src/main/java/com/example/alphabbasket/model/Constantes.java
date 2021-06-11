d7f17010d85d872cd1920db28dfcec82394226ea
IrvLH <50757832+IrvLH@users.noreply.github.com>
2021-06-11 03:23:50 -0500

    Add files via upload

--- AlphaBbasket/app/src/main/java/com/example/alphabbasket/model/Constantes.java
+++ AlphaBbasket/app/src/main/java/com/example/alphabbasket/model/Constantes.java
@@ -3,7 +3,8 @@ package com.example.alphabbasket.model;
 public class Constantes {
     public static final String claveEncriptacion = "secreto!";
 
-    public static final String clientes = "http://192.168.1.102/API/clientes.php";
-    public static final String direcciones = "http://192.168.1.102/API/direcciones.php";
+    public static final String clientes = "http://192.168.1.105/API_BB/clientes.php";
+    public static final String direcciones = "http://192.168.1.105/API_BB/APIBB/direcciones.php";
+    public static final String tiendas = "http://192.168.1.105/API_BB/APIBB/tiendas.php";
 
 }
